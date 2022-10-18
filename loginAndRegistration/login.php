3b0ed4e426c8ff8eb42badbf5f64088ce6c1c0e3
Ryuzaki-L-07 <95172695+Ryuzaki-L-07@users.noreply.github.com>
2022-10-18 21:10:23 +0600

    Updated login page

--- loginAndRegistration/login.php
+++ loginAndRegistration/login.php
@@ -28,30 +28,128 @@ if(isset($_POST['submit'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+<title>KaajKormo - Login</title>
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Log in</title>
-  <link rel="stylesheet" href="css/styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <meta charset="UTS-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-sclae=1.0">
+    <link rel="stylesheet" href="../home/css/style.css">
+    <link rel="stylesheet" href="../loginAndRegistration/css/login_styles.css">
+    <!--font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!--font for logo-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!--icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
-  
-  <div class="container form-container">
-    <form action="" method="post">
-
-      <h3>Log In Now</h3>
+  <!--navbar starts-->
+  <div class="big-navbar">
+    <!-- navbar-1 starts although its name is navbar-->
+    <nav class="navbar">
+        <h2 class="navbar-logo"> <a href="index.html">Kajkormo.com</a></h2>
+        <div class="navbar-menu">
+            <a href="#jobs">Jobs</a>
+            <a href="#myjobs">Myjobs</a>
+            <a href="#elearning">E-learning</a>
+            <a href="#tender">Tender/Eoi</a>
+            <a href="#employers">Employers</a>
+        </div>
+        <div class="menu-toggle">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
+    <!-- navbar-1 ends -->
+    <!--navbar2 starts-->
+    <nav class="navbar2">
+        <div class="nb-class1">
+            <div class="sign-in">
+                <a href="../loginAndRegistration/login.php">Sign in</a><br>
+                <a href="../loginAndRegistration/register.php">Sign up</a>
+            </div>
+            <div class="career-resources-dropdown">
+                <select class="career-resource" id="career-resource">
+                    <option value="Career-Advice"><label for="Career-Advice">Career Advice</label></option>
+                    <option value="Career-Guidline"> <a href="#">Career Guideline</a></option>
+                    <option value="Interview-Tips"> <a href="#">Interview Tips</a></option>
+                    <option value="Resume-writing-tips"> <a href="#">Resume writing tips</a></option>
+                    <option value="Skill-development tips"> <a href="#">Skill development tips</a></option>
+                    <option value="Articles"> <a href="#">Articles</a></option>
+                </select>
+            </div>
+            <div class="contact-us">
+                <a href="#">Contact Us</a>
+            </div>
+        </div>
+        
+        <div class="nb-class2">
+            <div class="facebook">
+                <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+            </div>
+            <div class="linkedin">
+                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+            <div class="instagram">
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+            <div class="gmail">
+                <a href="#"> <i class="fa-solid fa-at"></i></a>
+            </div>
+            <div class="youtube">
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar-2 ends -->
+</div>
+<!-- navbar ends -->
+main - ER diagram
+1.contents - ki ki korsi
+2.Promised features
+3.kon kon features gula parsi
+4.gyan chart ey ki ki korsi
+  <div class="form-container">
+    <form class="form" action="" method="post">
+      <div class="login-title">
+        <h3>Log In Now</h3>
+      </div>
 
       <?php
       if(isset($error)) {
         echo '<span class="error-msg">'.$error.'</span>';
       }
       ?>
-
-      <input type="text" name="id" id="id" placeholder="Enter User ID" required>
-      <input type="password" name="password" id="password" placeholder="Enter password" required>
+      <div class="input-fields">
+        <input class="user-id" type="text" name="id" id="id" placeholder="Enter User ID" required>
+        <br>
+        <input class="password" type="password" name="password" id="password" placeholder="Enter password" required>
+      </div>
+      <br>
+      <div class="agree_policy">
+        <input type="checkbox" name="agree_to_terms" id="agree_to_terms">
+            I have agreed to the 
+            <a href="../home/terms_and_conditions.html">
+              terms and conditions
+            </a> 
+            and read the 
+            <a href="../home/privacy_policy.html">
+              privacy policy
+            </a>
+            .
+      </div>
+      <br>
       <button type="submit" name="submit" class="form-btn" onclick="showMessage()">Log in</button>
-
+      <br>
+      <br>
       <p>Don't have an account? <a href="register.php">Register now</a></p>
 
     </form>
--- loginAndRegistration/register.php
+++ loginAndRegistration/register.php
@@ -34,7 +34,7 @@ if(isset($_POST['submit'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Registration</title>
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css\style.css">
 </head>
 <body>
   
