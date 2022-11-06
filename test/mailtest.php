120953e1869657a375a9cb3601a3362489492fa5
Talha76 <mushfiqurrahman845@gmail.com>
2022-11-06 23:43:40 +0600

    Removed credentials from test

--- test/mailtest.php
+++ test/mailtest.php
@@ -16,10 +16,7 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp-relay.sendinblue.com';            //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'mushfiqurtalha@iut-dhaka.edu';         //SMTP username
-    $mail->Password   = 'dkOqEIRFv84TJLYK';                     //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
 
