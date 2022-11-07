e2f141236cbbbcd7fca6319ee9e86e82fea833c0
Talha76 <mushfiqurrahman845@gmail.com>
2022-11-07 18:11:51 +0600

    Updated email verification system

--- loginAndRegistration/register.php
+++ loginAndRegistration/register.php
@@ -9,8 +9,7 @@ if (isset($_POST['submit'])) {
   $password         = md5(filter_input(INPUT_POST, 'password'));
   $confirm_password = md5(filter_input(INPUT_POST, 'confirm_password'));
   
-  $user_db = new Database();
-  $user_db->connect('user_db');
+  $user_db = new Database('user_db');
 
   $query = "SELECT id, email FROM userID WHERE id = '$id' OR email = '$email'";
   $result = $user_db->query($query);
@@ -21,25 +20,37 @@ if (isset($_POST['submit'])) {
     $error = "Invalid email!";
   } elseif ($password != $confirm_password) {
     $error = "Passwords don't match.";
-  } else {
-    $insert = "insert into userID values('$id', '$email', '$password', 'employee')";
-    $user_db->update($insert);    
+  } else {  
     $_SESSION['id'] = $id;
 
-    $smtp_credentials = new Database();
-    $smtp_credentials->connect('smtp_credentials');
+    $smtp_credentials = new Database('smtp_credentials');
 
+    $query = "SELECT otp FROM otp_info WHERE id = '$id'";
+    $otp_processing_result = $smtp_credentials->query($query);
+    if($otp_processing_result->num_rows > 0) {
+      $otp_processing_result = $smtp_credentials->query("SELECT NOW() - creation_time as time_passed FROM otp_info WHERE id = '$id'");
+      $otp_processed = $otp_processing_result->fetch_array();
+      if($otp_processed['time_passed'] <= 300) {
+        die("An OTP is already being processed for $id");
+      } else {
+        $smtp_credentials->update("DELETE FROM otp_info WHERE id = '$id'");
+      }
+    }
+
+    $otp = rand(10000000, 1000000000);
+    $_SESSION['otp'] = $otp;
+    $smtp_credentials->update("INSERT INTO otp_info VALUES('$id', '$email', '$password', 'employee', $otp, NOW())");
+    
+    $mail = new Mailer();
     $result = $smtp_credentials->query("SELECT * FROM smtp_credentials");
     if($result->num_rows == 0)
       throw new Exception("SMTP credentials not found");
     $row = $result->fetch_array();
-
-    $mail = new Mailer();
     $mail->connect($row['email'], $row['password']);
     $mail->setRecipients($email, $id);
     $mail->setSubject('Verify your Kaajkormo account.');
-    $mail->setBody("Hi" . $id . "!\nPlease verify your account now.");
-    $mail->setAltBody("Hi" . $id . "!\nPlease verify your account now.");
+    $mail->setBody("<p>Hi" . $id . "!</p><br><p>Your otp is $otp. Your otp will expire within 5 minutes.</p>");
+    $mail->setAltBody("Hi" . $id . "! Your otp is $otp. Your otp will expire within 5 minutes.");
     $mail->send();
     header('location:./registration_mail_verification.php');
   }
@@ -131,21 +142,21 @@ if (isset($_POST['submit'])) {
         <h3>Register Now</h3>
       </div>
 
-      <span id="error_msg">
-        <?php
-        if (isset($error)) {
-          echo $error;
-        }
-        ?>
+      <?php
+      if (isset($error)) {
+        echo '<span style="display: block; color: red;">'. $error .'</span>';
+      }
+      ?>
+      <span id="error_msg" class="error-msg">
       </span>
       <div class="input-fields">
-        <input id="id" class="user-id" type="text" name="id" placeholder="Choose user ID">
+        <input id="id" class="user-id" type="text" name="id" placeholder="Choose user ID" required>
         <br>
-        <input id="email" class="email" type="email" name="email" placeholder="Enter user email">
+        <input id="email" class="email" type="email" name="email" placeholder="Enter user email" required>
         <br>
-        <input id="password" class="enter-password" type="password" name="password" placeholder="Enter password">
+        <input id="password" class="enter-password" type="password" name="password" placeholder="Enter password" required>
         <br>
-        <input id="confirm_password" class="confirm-password" type="password" name="confirm_password" placeholder="Confirm password">
+        <input id="confirm_password" class="confirm-password" type="password" name="confirm_password" placeholder="Confirm password" required>
         <div class="pass-requirements">Password should contain at least:
           <ul>
             <li>8 characters.</li>
--- loginAndRegistration/registration_mail_verification.php
+++ loginAndRegistration/registration_mail_verification.php
@@ -2,6 +2,43 @@
 
 include '../phpDependencies/config.php';
 
+if(isset($_POST['submit'])) {
+  $conn = new Database('smtp_credentials');
+  
+  try {
+    $otp = filter_input(INPUT_POST, 'otp', FILTER_VALIDATE_INT);
+    $otp_result = $conn->query("SELECT * FROM otp_info WHERE id = '" . $_SESSION['id'] . "'");
+    $otp_row = $otp_result->fetch_array();
+    if($otp_result->num_rows == 0) {
+      $error = "No otp information available";
+    } else {
+      $time_difference_result = $conn->query("SELECT NOW() - creation_time as time_diff FROM otp_info WHERE id = '" . $_SESSION['id'] . "'");
+      $time_difference_row = $time_difference_result->fetch_array();
+      $time_difference = intval($time_difference_row['time_diff']);
+      $expected_otp = $otp_row['otp'];
+      if($time_difference > 300) {
+        $error = "OTP expired";
+        unset($_SESSION['id']);
+      } else if($otp != $expected_otp) {
+        $error = "OTP does not match";
+      } else {
+        $conn->update("DELETE FROM otp_info WHERE id = '" . $_SESSION['id'] . "'");
+
+        $conn->connect('user_db');
+        $id = $otp_row['id'];
+        $email = $otp_row['email'];
+        $password = $otp_row['password'];
+        $user_type = $otp_row['user_type'];
+        $insert = "INSERT INTO userID VALUES('$id', '$email', '$password', '$user_type')";
+        $conn->query($insert);
+        header('location:../home/index.php');
+      }
+    }
+  } catch(Exception $e) {
+    die("Error: " . $e->getMessage());
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +50,15 @@ include '../phpDependencies/config.php';
   <title>Verify your email</title>
 </head>
 <body>
-  <p>Hi <?php echo $_SESSION['id']; ?>!. A mail has been sent to you. Please check the inbox, if not found in inbox please check your spam folder.</p>
+  <a href="./register.php">Kaajkormo</a>
+  <p>
+  <?php
+  if(isset($error))
+    echo $error;
+  ?></p>
+  <form action="" method="post">
+    <input type="number" name="otp" id="otp">
+    <input type="submit" name="submit" value="Submit">
+  </form>
 </body>
 </html>
